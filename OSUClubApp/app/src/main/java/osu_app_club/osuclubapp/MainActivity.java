834dce9fdc74d3f6253e82cdf0278652bb18246d
Chris Newman <cnewman.robozinc@gmail.com>
2015-02-02 19:26:05 -0800

    Test commit 2

--- OSUClubApp/app/src/main/java/osu_app_club/osuclubapp/MainActivity.java
+++ OSUClubApp/app/src/main/java/osu_app_club/osuclubapp/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-// This is Chris Newman's branch! :D
+// This is Chris Newman's branch!
 
 public class MainActivity extends ActionBarActivity {
 
