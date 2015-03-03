63f0fc15fa56efa3556fada1392baa6e3e84e98e
Chris Newman <cnewman.robozinc@gmail.com>
2015-03-02 20:10:45 -0800

    Splash moves to main activity after animation

--- OSUClubApp/app/src/main/java/osu_app_club/osuclubapp/splash.java
+++ OSUClubApp/app/src/main/java/osu_app_club/osuclubapp/splash.java
@@ -17,8 +17,18 @@ public class Splash extends Activity{
         setContentView(R.layout.activity_splash);
         ImageView iv = (ImageView)findViewById(R.id.imageView);
         Animation animation = AnimationUtils.loadAnimation(this, R.anim.abc_fade_in);
-        animation.setDuration(6000);
+        animation.setDuration(3000);
         iv.startAnimation(animation);
-        //startActivity(new Intent(Splash.this, MainActivity.class));
+        animation.setAnimationListener(new Animation.AnimationListener(){
+            public void onAnimationStart(Animation a){
+            }
+            public void onAnimationRepeat(Animation a){
+            }
+            public void onAnimationEnd(Animation a){
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
     }
 }
